401366f987f2deb9d846752af233efd8f2ce5932
Arcelia Aquarius <ahermosillo@ugastro.berkeley.edu>
2018-03-02 18:18:35 -0800

    added Power averaging and plotting functions

--- aquariusLab2/lab2.py
+++ aquariusLab2/lab2.py
@@ -47,7 +47,7 @@ def make_complex(data, nblocs):
     imag = np.invert(real)
     return split[real] + 1j*split[imag]
 
-def transform_2D_3D(data, nblocs, nchunks, N):
+def transform_2D_3D(data, nblocs, nchunks, nsamples = 16000):
     '''
     Transforms 2d array to 3d array
 
@@ -60,11 +60,26 @@ def transform_2D_3D(data, nblocs, nchunks, N):
     Returns:
     3D array with shape (nblocs, nchunks, N)
     '''
-    if len(data[0,:]) % nchunks != 0:
-        raise Exception("Cannot split into these number of chunks. Try a divisor of {}".format(len(data[0,:])))
+    if nsamples % nchunks != 0:
+        raise Exception("Cannot split into these number of chunks. Try a divisor of {}".format(nsamples))
     else:
-        new_comp = data.reshape((nblocs, nchunks, N/nchunks))
+        new_comp = data.reshape((nblocs, nchunks, nsamples/nchunks))
     return new_comp
+
+def frequencies(div, N):
+    '''
+    Array of frequencies for given sampling frequency and N number of points
+    Arguments: 
+    div: (int) divisor chosen when collecting data with pico sampler to determing sampling frequency
+    N: (int) number of points 
+
+    Returns:
+    Array of frequencies of length N
+    '''
+
+    vsamp = 62.5/div
+    freq = np.linspace(-vsamp/2, (vsamp/2)*(1-2./N), N)
+    return freq
 
 def power(data,N):
     '''
@@ -81,8 +96,28 @@ def power(data,N):
     power = abs(fft_)**2
     return np.fft.fftshift(power)
 
+def avg_power(compData, div, N):
+    vsamp = 62.5/div
+    freq = np.linspace(-vsamp/2, (vsamp/2)*(1-2./N),N)
+    sum_power = 0
+    for i in range(len(compData)):
+        fft_ = np.fft.fft(compData[i][:N])
+        sum_power += abs(fft_)**2
+    sum_power = sum_power/len(compData)
+    return freq, np.fft.fftshift(sum_power)
 
 def avg_3d_power(data, nblock, nchunk, nsample, N, div = 1):
+    samp_len = nsample/nchunk
+
+    vsamp = 62.5/div
+    freq = np.linspace(-vsamp/2, (vsamp/2)*(1-2./samp_len), samp_len)
+
+    comp_3D = transform_2D_3D(data, nblock, nchunk, nsamples=nsample)
+    newpower = power(comp_3D, N)
+    power_avg = (newpower.sum(axis = 1)/nchunk).sum(axis =0)/nblock
+    return freq, power_avg
+
+def avg_3d_power_plot(data, nblock, nchunk, nsample, N, color, lab, div = 1):
     '''
     Calculates averaged power spectra for data split into chunks in a 3d array
 
@@ -93,6 +128,9 @@ def avg_3d_power(data, nblock, nchunk, nsample, N, div = 1):
     nsample: (int) number of samples originally
     N: (int) number of samples to use
     div: (int) divisor set when collecting the data
+    
+    Returns:
+    plot of the averaged Power using the 3D array
     '''
 
     samp_len = nsample/nchunk
@@ -100,14 +138,16 @@ def avg_3d_power(data, nblock, nchunk, nsample, N, div = 1):
     vsamp = 62.5/div
     freq = np.linspace(-vsamp/2, (vsamp/2)*(1-2./samp_len), samp_len)
     
-    comp_3D = transform_2D_3D(d1420_comp, nblock, nchunk, nsample)
-    newpower = power(comp_3D,1, N)
+    comp_3D = transform_2D_3D(data, nblock, nchunk, nsamples=nsample)
+    newpower = power(comp_3D, N)
     power_avg = (newpower.sum(axis = 1)/nchunk).sum(axis =0)/nblock
-    plt.plot(freq, np.fft.fftshift(power_avg), label = 'average')
-    plt.xlim(-3,3)
+    plt.plot(freq, power_avg, '{}.-'.format(color), label = '{}'.format(lab))
+    plt.xlabel('Frequency MHz', fontsize = 15)
+    plt.ylabel('Power', fontsize = 15)
+    plt.tick_params(labelsize = 15)
+    plt.grid()
 
-
-def avg_power(compData, div, N):
+def avg_power_plot(compData, div, N, color, lab):
     '''
     Calculate the average power for data
     
@@ -127,9 +167,10 @@ def avg_power(compData, div, N):
         fft_ = np.fft.fft(compData[i][:N])
         sum_power += abs(fft_)**2
     sum_power = sum_power/len(compData)
-    plt.plot(freq, np.fft.fftshift(sum_power), 'k.-')
-    plt.xlabel('Frequencies MHz')
-    plt.ylabel('Power')
+    plt.plot(freq, np.fft.fftshift(sum_power), '{}.-'.format(color), label = '{}'.format(lab))
+    plt.xlabel('Frequency MHz', fontsize = 15)
+    plt.ylabel('Power', fontsize = 15)
+    plt.tick_params(labelsize = 15)
     plt.grid()
     
     print("Sampling at a frequency {}".format(vsamp))
@@ -163,3 +204,184 @@ def plot_power(compData, div, N):
     
     print("Sampling at a frequency {}".format(vsamp))
     print("Sampling with {} number of samples".format(N))
+
+## ======================= Coordinate Conversions ==========================================
+
+'''
+az = azimuth 
+alt = altitude
+LST = Local Sidereal TIme 
+Phi = Terrestrial altitude 
+ha = LST - ra
+dec = declination 
+ra = right ascension 
+l = longitude galactic coordinate 
+b = langitude galactic coordinate
+'''
+
+
+def convert_az_alt_to_ha_dec(az,alt,phi):
+    x0 = np.cos(np.radians(alt))*np.cos(np.radians(az))
+    x1 = np.cos(np.radians(alt))*np.sin(np.radians(az))
+    x2 = np.sin(np.radians(alt))
+
+    R = np.matrix([[-np.sin(np.radians(phi)),0,np.cos(np.radians(phi))],[0,-1,0],[np.cos(np.radians(phi)),0,np.sin(np.radians(phi))]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(np.transpose(R),x)
+
+    ha = float(np.degrees(np.arctan2(xp[1],xp[0]))) + 360
+    dec = float(np.degrees(np.arcsin(xp[2]))) 
+
+    return ha, dec
+
+def convert_ha_dec_to_az_alt(ha,dec,phi):
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ha))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ha))
+    x2 = np.sin(np.radians(dec))
+
+    R = np.matrix([[-np.sin(np.radians(phi)),0,np.cos(np.radians(phi))],[0,-1,0],[np.cos(np.radians(phi)),0,np.sin(np.radians(phi))]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(R,x)
+
+    az = float(np.degrees(np.arctan2(xp[1],xp[0]))) 
+    alt = float(np.degrees(np.arcsin(xp[2]))) 
+
+    return az, alt
+
+def convert_ra_dec_to_ha_dec(ra,dec,LST):
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ra))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ra))
+    x2 = np.sin(np.radians(dec))
+
+    R = np.matrix([[np.cos(np.radians(LST)),np.sin(np.radians(LST)),0],[np.sin(np.radians(LST)),-np.cos(np.radians(LST)),0],[0,0,1]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(R,x)
+
+    ha = float(np.degrees(np.arctan2(xp[1],xp[0])))
+    dec = float(np.degrees(np.arcsin(xp[2])))
+    return ha, dec
+
+def convert_ha_dec_to_ra_dec(ha,dec,LST):
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ha))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ha))
+    x2 = np.sin(np.radians(dec))
+        
+    R = np.matrix([[np.cos(np.radians(LST)),np.sin(np.radians(LST)),0],[np.sin(np.radians(LST)),-np.cos(np.radians(LST)),0],[0,0,1]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(np.transpose(R),x)
+
+    ra = float(np.degrees(np.arctan2(xp[1],xp[0])))
+    dec = float(np.degrees(np.arcsin(xp[2])))
+    return ra, dec
+
+def convert_ra_dec_to_galactic1950(ra,dec): 
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ra))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ra))
+    x2 = np.sin(np.radians(dec))
+
+    R = np.matrix([[-0.066989,-0.872756,-0.483539],[0.492728,-0.450347,0.744585],[-0.867601,-0.188375,0.460200]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(R,x)
+
+    l = float(np.degrees(np.arctan2(xp[1],xp[0]))) + 360
+    b = float(np.degrees(np.arcsin(xp[2])))
+
+    return l ,b
+
+def convert_galactic1950_to_ra_dec(l,b): 
+    x0 = np.cos(np.radians(b))*np.cos(np.radians(l))
+    x1 = np.cos(np.radians(b))*np.sin(np.radians(l))
+    x2 = np.sin(np.radians(b))
+
+    R = np.matrix([[-0.066989,-0.872756,-0.483539],[0.492728,-0.450347,0.744585],[-0.867601,-0.188375,0.460200]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(np.transpose(R),x)
+
+    ra =  float(np.degrees(np.arctan2(xp[1],xp[0])))
+    dec = float(np.degrees(np.arcsin(xp[2])))
+
+    return ra, dec
+
+def convert_ra_dec_to_galactic2000(ra,dec): 
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ra))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ra))
+    x2 = np.sin(np.radians(dec))
+
+    R = np.matrix([[-0.054876,-0.873437,-0.483835],[0.494109,-0.444830,0.746982],[-0.867666,-0.198076,0.455984]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(R,x)
+
+    l = float(np.degrees(np.arctan2(xp[1],xp[0])) ) + 360
+    b = float(np.degrees(np.arcsin(xp[2])))
+
+    return l,b
+
+def convert_galactic2000_to_ra_dec(l,b): 
+    x0 = np.cos(np.radians(b))*np.cos(np.radians(l))
+    x1 = np.cos(np.radians(b))*np.sin(np.radians(l))
+    x2 = np.sin(np.radians(b))
+
+    R = np.matrix([[-0.054876,-0.873437,-0.483835],[0.494109,-0.444830,0.746982],[-0.867666,-0.198076,0.455984]])
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(np.transpose(R),x)
+
+    ra =  float(np.degrees(np.arctan2(xp[1],xp[0])))
+    dec = float(np.degrees(np.arcsin(xp[2])))
+
+    return ra, dec
+
+
+def convert_ra_dec_to_az_alt(ra,dec,LST,phi):
+    x0 = np.cos(np.radians(dec))*np.cos(np.radians(ra))
+    x1 = np.cos(np.radians(dec))*np.sin(np.radians(ra))
+    x2 = np.sin(np.radians(dec))
+
+    R0 = np.matrix([[np.cos(np.radians(LST)),np.sin(np.radians(LST)),0],[np.sin(np.radians(LST)),-np.cos(np.radians(LST)),0],[0,0,1]])
+    R1 = np.matrix([[-np.sin(np.radians(phi)),0,np.cos(np.radians(phi))],[0,-1,0],[np.cos(np.radians(phi)),0,np.sin(np.radians(phi))]])
+
+
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(R1,np.dot(R0,x))
+
+    az =  float(np.degrees(np.arctan2(xp[1],xp[0])))
+    alt = float(np.degrees(np.arcsin(xp[2])))
+
+    return az, alt
+
+
+def convert_az_alt_to_ra_dec(az,alt,LST,phi):
+    x0 = np.cos(np.radians(alt))*np.cos(np.radians(az))
+    x1 = np.cos(np.radians(alt))*np.sin(np.radians(az))
+    x2 = np.sin(np.radians(alt))
+
+    R0 = np.matrix([[np.cos(np.radians(LST)),np.sin(np.radians(LST)),0],[np.sin(np.radians(LST)),-np.cos(np.radians(LST)),0],[0,0,1]])
+    R1 = np.matrix([[-np.sin(np.radians(phi)),0,np.cos(np.radians(phi))],[0,-1,0],[np.cos(np.radians(phi)),0,np.sin(np.radians(phi))]])
+
+
+
+    x = np.matrix([[x0],[x1],[x2]])
+
+    xp = np.dot(np.transpose(R0),np.dot(np.transpose(R1),x))
+
+    ra =  float(np.degrees(np.arctan2(xp[1],xp[0])))
+    dec = float(np.degrees(np.arcsin(xp[2])))
+
+    return ra, dec
